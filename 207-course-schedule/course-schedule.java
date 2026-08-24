cc789b56fba46ed702a5e7c26f3562cb16dd2a1b
Sak1105 <sakshiyadav11052005@gmail.com>
2026-08-24 21:18:38 +0530

    Time: 29 ms (5.03%) | Memory: 46.1 MB (99.71%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,58 +1,32 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         int n=numCourses;
-         int[] indegree =new int[n];
-        int[] outdegree =new int[n];
-        int[][] adj=new int[n][n];
-        int len=prerequisites.length;
-       for(int i = 0; i < prerequisites.length; i++) {
-    int course = prerequisites[i][0];
-    int prerequisite = prerequisites[i][1];
-
-    adj[prerequisite][course] = 1;
-}
-        for(int i=0;i<n;i++){
-           for(int j=0;j<n;j++){
-            if(adj[i][j]==1){
-                indegree[j]++;
-                outdegree[i]++;
-            }
-           }
+        int count=0;
+        int[] indegree=new int[n];
+        for(int i=0;i<prerequisites.length;i++){
+            indegree[prerequisites[i][1]]++;
         }
         Queue<Integer> q=new LinkedList<>();
-        List<Integer> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
                 q.add(i);
             }
         }
         while(!q.isEmpty()){
-        int temp=q.poll();
-        ans.add(temp);
-        for(int j=0;j<n;j++){
-            if(adj[temp][j]!=0){
-                indegree[j]--;
-             if(indegree[j]==0){
-                q.add(j);
-            }
-            }
-        }
-        
-        }
-        boolean flag=false;
-         if(ans.size()!=n){
-            return false;
-         }
-        return true;
-        
-    }
-    private static boolean allZero(int[] indegree){
-        int n=indegree.length;
-        for(int i=0;i<n;i++){
-            if(indegree[i]!=0){
-                return false;
+            int temp=q.poll();
+            count++;
+            for(int i=0;i<prerequisites.length;i++){
+                if(prerequisites[i][0]==temp){
+                    indegree[prerequisites[i][1]]--;
+                    if( indegree[prerequisites[i][1]]==0){
+                        q.add(prerequisites[i][1]);
+                    }
+                }
             }
         }
-        return true;
+        if(count==numCourses){
+            return true;
+        }
+        return false;
     }   
 }
